1741d1afada0357d0a4e28f8124486dd6f13e0bb
Ben Pantazon <pantazon-b@live.com>
2020-07-05 19:31:26 -0500

    feat: first commit, contains working app

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+import useLocation from './useLocation';
+
+const App = () => {
+    const [lat, errorMessage] = useLocation();
+    let content;
+    if(errorMessage) {
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat){
+        content = <SeasonDisplay latitude={lat} />
+    } else {
+        content = <Loader message="Please allow location :)"/>
+    }
+
+    return <div className="border red">{content}</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
